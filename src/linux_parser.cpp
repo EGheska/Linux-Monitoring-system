9df645b6a0d66b1a6d6ed5c799a698cec98dd6f7
EGheska <dimanich300@gmail.com>
2023-01-14 19:01:40 +0100

    CPU states defined in linux_parcer and shown in the Processor's constructor

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -1,10 +1,12 @@
 //
 // Created by Dima on 14/01/2023.
 //
-
-#include "../inc/linux_parser.h"
 #include <string>
 #include <vector>
+#include <dirent.h>
+#include <unistd.h>
+
+#include "../inc/linux_parser.h"
 
 float LinuxParser::MemoryUtil() {
     return 0.0;
@@ -45,11 +47,47 @@ std::string LinuxParser::Kernel() {
 }
 
 std::string LinuxParser::OS() {
-    return std::string();
+    std::string line;
+    std::string key;
+    std::string value;
+    std::ifstream filestream(kOSPath);
+
+    // Read the file line by line
+    if(filestream.is_open()){
+        while(std::getline(filestream, line)){
+            // Parse the line
+            std::replace(line.begin(), line.end(), '=', ' ');
+            std::replace(line.begin(), line.end(), '"', ' ');
+            std::istringstream linestream(line);
+            // Read the line for key value pairs and return it
+            while(linestream >> key >> value){
+                if(key == "PRETTY_NAME"){
+                    std::replace(value.begin(), value.end(), '_', ' ');
+                    return value;
+                }
+            }
+        }
+    }
+    return value;
 }
 
 std::vector<std::string> LinuxParser::CpuUtil() {
-    return std::vector<std::string>();
+    std::string line;
+    std::string key;
+    std::string value;
+    std::string usertime, nicetime, systemtime, idletime, iowait, irq, softirq, steal;
+    std::ifstream filestream(kProcDirectory + kStatFilename);
+    if (filestream.is_open()) {
+        while (std::getline(filestream, line)) {
+            std::istringstream linestream(line);
+            while (linestream >> key >> usertime >> nicetime >> systemtime >> idletime >> iowait >> irq >> softirq >> steal) {
+                if (key == "cpu") {
+                    return {usertime, nicetime, systemtime, idletime, iowait,   irq,      softirq,    steal};
+                }
+            }
+        }
+    }
+    return {};
 }
 
 // Read and return the system memory used by the process
--- src/processor.cpp
+++ src/processor.cpp
@@ -2,7 +2,28 @@
 // Created by Dima on 14/01/2023.
 //
 #include "../inc/processor.h"
+#include "../inc/linux_parser.h"
+
+#include "string"
+#include "vector"
+
 
 float Processor::Utilization() {
+    // Return the aggregate CPU utilizations
+    std::vector<std::string> cpuUtil = LinuxParser::CpuUtil();
+    const long int usertime = std::stol(cpuUtil[0]);
+    const long int nicetime = std::stol(cpuUtil[1]);
+    const long int systemtime = std::stol(cpuUtil[2]);
+    const long int idletime = std::stol(cpuUtil[3]);
+    const long int iowait = std::stol(cpuUtil[4]);
+    const long int irq = std::stol(cpuUtil[5]);
+    const long int softirq = std::stol(cpuUtil[6]);
+    const long int steal = std::stol(cpuUtil[7]);
+
+    // Calculate needed current CPU's states values
+    const float IDLE = idletime + iowait;
+    const float NON_IDLE = usertime + nicetime + systemtime + irq + softirq + steal;
+    const float TOTAL = IDLE + NON_IDLE;
+
 
 }

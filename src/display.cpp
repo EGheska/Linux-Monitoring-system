d82577f2e8f26e805d993d492d2e2c5d4ec7f129
jurijjzz <jurijszuravlovs@gmail.com>
2023-01-25 17:10:56 +0100

    removed colors

--- src/display.cpp
+++ src/display.cpp
@@ -11,21 +11,12 @@
 #include "display.h"
 
 
-// 50 bars uniformly displayed from 0 - 100 %
-// 2% is one bar(|)
+
 std::string NCursesDisplay::ProgressBar(float percent) {
-  std::string result{""};
-  int size{50};
-  float bars{percent * size};
-
-  for (int i{0}; i < size; ++i) {
-    result += i <= bars ? '|' : ' ';
-  }
-
   std::string display{std::to_string(percent * 100).substr(0, 4)};
   if (percent < 0.1 || percent == 1.0)
     display = "" + std::to_string(percent * 100).substr(0, 3);
-  return result + "" + display + "/100%";
+  return display + "/100%";
 }
 
 void NCursesDisplay::DisplaySystem(System& system, WINDOW* window) {
@@ -33,43 +24,33 @@ void NCursesDisplay::DisplaySystem(System& system, WINDOW* window) {
   mvwaddstr(window, ++row, 2, ("OS: " + system.OperatingSystem()).c_str());
   mvwaddstr(window, ++row, 2, ("Kernel: " + system.Kernel()).c_str());
   mvwaddstr(window, ++row, 2, "CPU:");
-  wattron(window, COLOR_PAIR(1));
   mvwaddstr(window, row, 9, " ");
   waddstr(window, ProgressBar(system.Cpu().Utilization()).c_str());
-  wattroff(window, COLOR_PAIR(1));
   mvwaddstr(window, ++row, 2, "Memory:  ");
-  wattron(window, COLOR_PAIR(1));
   mvwaddstr(window, row, 10, "");
   waddstr(window, ProgressBar(system.MemoryUtilization()).c_str());
-  wattroff(window, COLOR_PAIR(1));
-  mvwaddstr(window, ++row, 2,
-            ("Total Processes: " + std::to_string(system.TotalProcesses())).c_str());
-  mvwaddstr(
-      window, ++row, 2,
-      ("Running Processes: " + std::to_string(system.RunningProcesses())).c_str());
-  mvwaddstr(window, ++row, 2,
-            ("Up Time: " + Format::ElapsedTime(system.UpTime())).c_str());
+  mvwaddstr(window, ++row, 2,("Total Processes: " + std::to_string(system.TotalProcesses())).c_str());
+  mvwaddstr(window, ++row, 2,("Running Processes: " + std::to_string(system.RunningProcesses())).c_str());
+  mvwaddstr(window, ++row, 2,("Up Time: " + Format::ElapsedTime(system.UpTime())).c_str());
   wrefresh(window);
 }
 
 void NCursesDisplay::Display(System& system) {
+  // initizalize ncurses -screen, no echo, no delay, no cursor
   initscr();      // start ncurses
   noecho();       // do not print input values
   cbreak();       // terminate ncurses on ctrl + c
-  start_color();  // enable color
 
   int x_max{getmaxx(stdscr)};
   WINDOW* system_window = newwin(9, x_max - 1, 0, 0);
 
   while (1) {
-    init_pair(1, COLOR_BLUE, COLOR_BLACK);
-
     box(system_window, 0, 0);
 
     DisplaySystem(system, system_window);
 
     wrefresh(system_window);
-
+    // refresh screen to match memory
     refresh();
     std::this_thread::sleep_for(std::chrono::seconds(1));
   }
